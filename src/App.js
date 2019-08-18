c2696a083b2a9575488bbcbd6f5807414b50e1ee
JIA YEE <jiayeerawr@gmail.com>
2019-08-18 22:56:15 +0800

    Add map

--- src/App.js
+++ src/App.js
@@ -10,30 +10,25 @@ import {
   MapPage
 } from './pages';
 
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/map">Map</Link>
-              </li>
-            </ul>
-            <hr />
-            <Route exact path="/" component={IndexPage} />
-            <Route path="/map" component={MapPage} />
-          </div>
-        </Router>
-      </header>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/map">Map</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={IndexPage} />
+          <Route path="/map" component={MapPage} />
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import L from 'leaflet';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import './MapContainer.scss';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const mapUrl = 'https://maps-{s}.onemap.sg/v3/Default/{z}/{x}/{y}.png';
+const mapAttribution = '<img src="https://docs.onemap.sg/maps/images/oneMap64-01.png" style="height:20px;width:20px;"/> New OneMap | Map data &copy; contributors, <a href="http://SLA.gov.sg">Singapore Land Authority</a>';
 
 class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: [51.505, -0.09]
+    };
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition((position) => {
+        this.setState({
+          position: [
+            position.coords.latitude,
+            position.coords.longitude
+          ]
+        });
+      });
+    }
+  }
+
   render() {
     return (
       <div className="MapContainer">
-        MapContainer
+        <Map
+          id="map"
+          maxBounds={[[1.56073, 104.1147], [1.16, 103.502]]}
+          center={this.state.position}
+          zoom={18}
+        >
+          <TileLayer
+            url={mapUrl}
+            attribution={mapAttribution}
+          />
+          <Marker position={this.state.position}>
+            <Popup>You are here.</Popup>
+          </Marker>
+        </Map>
       </div>
     );
   }
