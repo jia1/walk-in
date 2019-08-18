94ff73e2d259808e141d2956b079f30eeef0ac2b
JIA YEE <jiayeerawr@gmail.com>
2019-08-19 00:18:37 +0800

    Use material UI

--- src/App.js
+++ src/App.js
@@ -10,24 +10,66 @@ import {
   MapPage
 } from './pages';
 
-import './App.css';
+import {
+  AppBar,
+  Container,
+  CssBaseline,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography
+} from '@material-ui/core';
+import {
+  Home as HomeIcon,
+  Place as PlaceIcon
+} from '@material-ui/icons';
+import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
+      <CssBaseline />
+      <AppBar position="fixed" className="AppBar">
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Wålk IN
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/map">Map</Link>
-            </li>
-          </ul>
+      <Drawer
+        className="Drawer"
+        variant="permanent"
+        classes={{
+          paper: "DrawerPaper",
+        }}
+      >
+        <Toolbar />
+        <List>
+          <ListItem button component={Link} to="/">
+            <ListItemIcon><HomeIcon /></ListItemIcon>
+            <ListItemText primary="Index" />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem button component={Link} to="/map">
+            <ListItemIcon><PlaceIcon /></ListItemIcon>
+            <ListItemText primary="Map" />
+          </ListItem>
+        </List>
+      </Drawer>
+      <main className="Content">
+        <Toolbar />
+        <Container maxWidth="xl">
           <Route exact path="/" component={IndexPage} />
           <Route path="/map" component={MapPage} />
-        </div>
+        </Container>
+      </main>
       </Router>
     </div>
   );
